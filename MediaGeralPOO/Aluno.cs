ea22e0a1423dbf3f7913a016128dfbb4c8116f1c
User <User@DESKTOP-OFTTR4B>
2022-04-29 17:46:20 -0300

    Adicionar arquivos de projeto.

---
+++ MediaGeralPOO/Aluno.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Aluno
+    {
+        //Com o get e set, podemos obter os valores a partir do objeto com o Get e definir os valores com o set;
+        public string Nome { get; private set; } //privamos o "set" para que os nomes dos alunos, não sejam alterados.
+
+        //Um array que irá guardar as notas dos alunos
+        private double[] _notas; 
+
+        //Propriedade que irá retornar a média das notas;
+        public double Media 
+        {
+            get
+            {
+                return CalcularMedia();
+            }
+        }
+
+        //Um método construtor que irá guardar o nome do nosso aluno e a quantidade de provas;
+        public Aluno(string nome, int provas)
+        {
+            Nome = nome;
+            _notas = new double[provas]; //Aqui estamos definindo o tamanho do Array, com a quantidade de provas. Caso fez 2 provas, são 2 notas; 
+
+        }
+
+        //Método para ser inseridas as notas;
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++) //Essa propriedade ".Length está sendo utilizada para dizer/transparecer a quantidade de provas;
+            {
+                Console.Write("Nota #" + (i + 1) + ": ");
+                _notas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        //Método para calcular a média da nota deste aluno acima;
+        private double CalcularMedia()
+        {
+            double total = 0;
+            for(int i = 0; i < _notas.Length; i++)
+            {
+                total +=_notas[i];
+            }
+
+            return total / _notas.Length;
+        }
+    }
+}
---
+++ MediaGeralPOO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Título
+            Console.Title = "### Média Geral dos Alunos ###";
+
+            Console.WriteLine("Quantos alunos?");
+            int nAlunos = int.Parse(Console.ReadLine());
+
+            //Quebra de linha
+            Console.WriteLine(); 
+
+            //Um vetor do tipo Aluno (nossa classe) para preencher com os alunos;
+            Aluno[]alunos = new Aluno[nAlunos]; 
+            for(int i = 0; i < nAlunos; i++)
+            {
+                Console.Clear();
+                Console.Write("Aluno #" + (i + 1) + " Nome..: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Aluno #" + (i + 1) + " Provas: ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, provas);
+
+                //Método inserir notas do aluno
+                Console.WriteLine("Insira as notas do aluno " + nome);
+                alunos[i].InserirNotas();
+            }
+
+            //Limpar a tela
+            Console.Clear(); 
+
+            double mediaGeral = 0;
+            foreach(Aluno aluno in alunos) //A cada passagem da nossa variável "aluno", será atribuido um elemento do nosso array;
+            {
+                Console.WriteLine("Aluno: " + aluno.Nome);
+                Console.WriteLine("Media: " + aluno.Media);
+                Console.WriteLine();
+
+                mediaGeral += aluno.Media;
+            }
+
+            double resultadoFinal = mediaGeral / alunos.Length;
+
+            Console.WriteLine("Média geral dos alunos: " + resultadoFinal);
+            Console.ReadKey();
+        }
+    }
+}
